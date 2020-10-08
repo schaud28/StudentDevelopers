b5f434f3f25682ae05e4522c7ba6a53b8fecb91a
schaud28 <schaud28@asu.edu>
2020-10-07 23:09:18 -0700

    Dockerized the backend and connected mongoDb

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
+// importing dependencies
 const express = require('express');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const http = require('http');
+
+// expressjs
 const app = express();
-const connectDB = require('./config/db');
 
-connectDB();
-app.get('/', (req, res) => res.send('API running'));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on prot ${PORT}`));
 
+
+// mongodb
+mongoose
+    .connect(
+        `mongodb://mongodb-service/cloudl`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
+
+// fireup the server
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Server up and running on port ${port} !`));
