964b77e810d3b7aa80a7516d96ff546c93bcea43
schaud28 <schaud28@asu.edu>
2020-10-07 22:43:54 -0700

    Added new code

--- server.js
+++ server.js
@@ -7,3 +7,6 @@ app.get('/', (req, res) => res.send('API running'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on prot ${PORT}`));
+
+
+console.log("Added new shit");
